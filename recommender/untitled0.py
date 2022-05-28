081b6c2fea55be612adf111a9200f3f8701b096f
Srishti Manna <srishtimanna6@gmail.com>
2022-05-28 21:51:53 +0530

    initial commit

---
+++ recommender/untitled0.py
@@ -0,0 +1,10 @@
+import csv
+import pandas as pd
+import random
+New=[]
+with open('movieR.csv','r') as csvfile:
+    readCSV = csv.reader(csvfile)
+    New.append(random.choice(list(readCSV)))
+print(New[0][0])
+        
+        
